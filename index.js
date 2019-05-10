a9e92c981094ab75cf8ecf6daa224fc72a8cd498
itsol <itsol>
2019-05-10 17:19:35 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
 
 const shopIdsByUsername = 'https://shopee.vn/api/v1/shop_ids_by_username/';
 
-const getCookie = (url) => {
+const getPage = (url) => {
     return new Promise((res, rej) => {
         request({
             url: url
@@ -27,13 +27,13 @@ const getMatchCookie = (item) => {
 }
 
 const convertCookieToJson = (arr, item) => {
+    item = item.replace(/"/gi, '');
     const arrSplit = item.split("=");
     arr[arrSplit[0]] = arrSplit[1];
     return arr;
 }
 
-
-const getIdsByUsername = (urlShop, cookie) => {
+const getCookieGlobal = (cookie) => {
     let arr = {};
     const cookieString = cookie.reduce((result, item, index) => {
         if (index == 1) {
@@ -45,6 +45,27 @@ const getIdsByUsername = (urlShop, cookie) => {
         arr = convertCookieToJson(arr, match);
         return result += match + ";";
     });
+    return {
+        cookie: cookieString,
+        cookieArray: arr
+    };
+}
+
+const getCookie = () => {
+    return new Promise(async (res, rej) => {
+        try {
+            const cookiePage = await getPage('https://shopee.vn/');
+            const result = getCookieGlobal(cookiePage);
+            return res(result);
+        } catch (error) {
+            return rej(error);
+        }
+    })
+
+}
+
+
+const getIdsByUsername = (urlShop, cookie) => {
 
 
     // console.log(cookie);
@@ -55,9 +76,7 @@ const getIdsByUsername = (urlShop, cookie) => {
         'x-csrftoken': arr.csrftoken,
         'Content-Type': 'text/plain'
     }
-    const body = {
-        usernames: ['kieungoc']
-    }
+
     return new Promise((res, rej) => {
         request({
             url: shopIdsByUsername,
@@ -85,34 +104,137 @@ const getList = (cookie, url) => {
     return new Promise((res, rej) => {
         request({
             url: url,
-            headers :{
+            headers: {
                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
                 'cookie': cookie.csrftoken,
             }
         }, (error, response, body) => {
-            if(error || response.statusCode != 200){
+            if (error || response.statusCode != 200) {
                 return rej(error);
             }
-
             return res(JSON.parse(body));
-            
+
         })
     })
 
 }
 
-const main = async () => {
-    const urlShop = 'https://shopee.vn/kieungoc';
-    const cookie = await getCookie(urlShop);
-
-    const idShopTemp = await getIdsByUsername(urlShop, cookie);
-    const idShop = 1481792;
-    const urlListItem = await getUrlListItem(idShop);
-    const list = await getList(idShopTemp.cookieObj, urlListItem);
-    console.log(list);
+const getDetailItem = (idShop, idItem) => {
+    const url = 'https://shopee.vn/api/v2/item/get?itemid='+idItem+'&shopid='+idShop;
 
 }
-main();
+
+const viewPage = (idShop, idItem) => {
+    return new Promise((res, rej) => {
+        request({
+            url: 'https://shopee.vn/__t__',
+            headers: {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+            },
+            body: '[null,null,"\"vsqvTEyW8bxm0gOnDYi5HLCrwYbXvSsaomr4ssIypxjYgt00tP3KrcZ6jAaLE/3yBD69Tj/oQOSoseJbZebV1iRf2wdcAjnm0J5INrNN6Z0\"",null,null,5,3,[[null,2207394496,110268868,null,false,false,null,null,null,null,[],null,null,null,null,null,null,null,null]],1557473530602,"VN","\"UUfADeS8wJalhPljDc2gQg==\"",null,[]]',
+            method: 'POST'
+        }, (error, response, body) => {
+            console.log(response.statusCode);
+        })
+    })
+}
+// viewPage();
+const main = async () => {
+    // const urlShop = 'https://shopee.vn/kieungoc';
+    // const cookiePage = await getCookie(urlShop);
+
+    // const idShopTemp = await getIdsByUsername(urlShop, cookie);
+    const cookie = await getCookie();
+    const idShop = 110268868;
+    const urlListItem = getUrlListItem(idShop);
+    const list = await getList(cookie.cookieArray, urlListItem);
+    console.log(list);
+    const itemPage = list.item;
+    // itemPage.forEach((item) => {
+
+    // })
+}
+//main();
+//const cookie = await getCookie();
+const search = async (key) => {
+    const url = 'https://shopee.vn/api/v2/search_items/?by=relevancy&keyword=' + key + '&limit=50&newest=0&order=desc&page_type=search';
+    return new Promise((res, rej) => {
+        request({
+            url: url,
+            headers: {
+                'user-agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+            }
+        }, (error, response, body) => {
+            return res(body);
+        })
+    })
+}
+
+
+// search('goi gu').then((data) => {
+//     const shop = data.item;
+//     shop.map((item) => {
+
+//     })
+// })
+
+const subscrible = async (shopId) => {
+    const cookie = await getCookie();
+    request({
+        url: 'https://shopee.vn/userstats_record',
+        headers: {
+
+        }
+    })
+}
+
+
+
+
+var headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+    'Content-Type': 'text/plain'
+};
+
+var dataString = '[null,null,"\"vsqvTEyW8bxm0gOnDYi5HLCrwYbXvSsaomr4ssIypxjYgt00tP3KrcZ6jAaLE/3yBD69Tj/oQOSoseJbZebV1iRf2wdcAjnm0J5INrNN6Z0\"",null,null,5,3,[[null,2207448661,110268868,null,false,false,null,null,null,null,[],null,null,null,null,null,null,null,null]],1557473530602,"VN","\"UUfADeS8wJalhPljDc2gQg==\"",null,[]]';
+
+var options = {
+    url: 'https://shopee.vn/__t__',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+// let data = JSON.stringify({
+//     password: this.state.password,
+//     username: this.state.email
+// })
+
+let data = ''
+const axios = require('axios')
+axios.post('https://shopee.vn/__t__', '[null,null,"\"vsqvTEyW8bxm0gOnDYi5HLCrwYbXvSsaomr4ssIypxjYgt00tP3KrcZ6jAaLE/3yBD69Tj/oQOSoseJbZebV1iRf2wdcAjnm0J5INrNN6Z0\"",null,null,5,3,[[null,2207448661,110268868,null,false,false,null,null,null,null,[],null,null,null,null,null,null,null,null]],1557473530602,"VN","\"UUfADeS8wJalhPljDc2gQg==\"",null,[]]', {
+    headers: {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+    }
+}).then(data => {
+    console.log(data)
+}).catch(error =>{
+    console.log(error)
+})
+// request(options, callback);
+
+
+
+
+
+
+
+
 
 
 // var headers = {
